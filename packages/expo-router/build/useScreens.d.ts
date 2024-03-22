33eca48d89372d5438237ba6545caa4e31532192
Mark Lawlor <mwlawlor@gmail.com>
2024-03-22 11:31:53 +1000

    update deps to react-navigation 7

--- packages/expo-router/build/useScreens.d.ts
+++ packages/expo-router/build/useScreens.d.ts
@@ -27,11 +27,11 @@ export type ScreenProps<TOptions extends Record<string, any> = Record<string, an
  */
 export declare function useSortedScreens(order: ScreenProps[]): React.ReactNode[];
 /** Wrap the component with various enhancements and add access to child routes. */
-export declare function getQualifiedRouteComponent(value: RouteNode): React.ComponentType<any> | React.ForwardRefExoticComponent<Omit<any, "ref"> & React.RefAttributes<unknown>>;
+export declare function getQualifiedRouteComponent(value: RouteNode): React.ForwardRefExoticComponent<Omit<any, "ref"> & React.RefAttributes<unknown>> | React.ComponentType<any>;
 /** @returns a function which provides a screen id that matches the dynamic route name in params. */
 export declare function createGetIdForRoute(route: Pick<RouteNode, 'dynamic' | 'route' | 'contextKey' | 'children'>): ({ params }?: {
     params?: Record<string, any> | undefined;
-}) => string;
-export declare function screenOptionsFactory(route: RouteNode, options?: ScreenProps['options']): RouteConfig<any, any, any, any, any>['options'];
+}) => any;
+export declare function screenOptionsFactory(route: RouteNode, options?: ScreenProps['options']): RouteConfig<any, any, any, any, any, any>['options'];
 export declare function routeToScreen(route: RouteNode, { options, ...props }?: Partial<ScreenProps>): React.JSX.Element;
 //# sourceMappingURL=useScreens.d.ts.map
