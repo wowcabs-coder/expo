dee58957c5058a4ad46894f0d099034dd1568864
Gabriel Donadel <donadeldev@gmail.com>
2024-06-20 18:13:12 -0300

    [network] Add details info to network state

--- packages/expo-network/ios/NetworkModule.swift
+++ packages/expo-network/ios/NetworkModule.swift
@@ -1,6 +1,7 @@
 import ExpoModulesCore
 import SystemConfiguration
 import Network
+import CoreTelephony
 
 public final class NetworkModule: Module {
   private let monitor = NWPathMonitor()
@@ -65,7 +66,7 @@ public final class NetworkModule: Module {
     return address
   }
 
-  private func getNetworkStateAsync() -> [String: Any] {
+  private func getNetworkStateAsync() -> [String: Any?] {
     let path = monitor.currentPath
     let isConnected = path.status == .satisfied
     var currentNetworkType = NetworkType.unknown
@@ -74,7 +75,8 @@ public final class NetworkModule: Module {
       return [
         "type": NetworkType.none.description,
         "isConnected": isConnected,
-        "isInternetReachable": isConnected
+        "isInternetReachable": isConnected,
+        "details": nil
       ]
     }
 
@@ -89,6 +91,8 @@ public final class NetworkModule: Module {
       currentNetworkType = .wifi
     case .cellular:
       currentNetworkType = .cellular
+    case .wiredEthernet:
+      currentNetworkType = .ethernet
     default:
       currentNetworkType = .unknown
     }
@@ -96,7 +100,106 @@ public final class NetworkModule: Module {
     return [
       "type": currentNetworkType.description,
       "isConnected": isConnected,
-      "isInternetReachable": isConnected
+      "isInternetReachable": isConnected,
+      "details": getDetailsForNetworkType(networkType: currentNetworkType)
     ]
+  }
+
+  private func getDetailsForNetworkType(networkType: NetworkType) -> [String: Any]? {
+    let path = monitor.currentPath
+    var details = ["isConnectionExpensive": path.isExpensive] as [String: Any]
+
+    switch networkType {
+    case .wifi,
+      .ethernet:
+      details["subnet"] = getSubnetMask()
+      return details
+    case .cellular:
+      details["cellularGeneration"] = getCellularGeneration()
+      return  details
+    default:
+      return nil
+    }
+  }
+
+  private func getCellularGeneration() -> NetworkCellularGeneration {
+    let networkInfo = CTTelephonyNetworkInfo()
+
+    guard let serviceCurrentRadioAccessTechnology = networkInfo.serviceCurrentRadioAccessTechnology else {
+      return NetworkCellularGeneration.unknown
+    }
+
+    for (_, radioTech) in serviceCurrentRadioAccessTechnology {
+      if #available(iOS 14.1, *) {
+        if radioTech == CTRadioAccessTechnologyNRNSA || radioTech == CTRadioAccessTechnologyNR {
+          return NetworkCellularGeneration.cellularGen5g
+        }
+      }
+
+      switch radioTech {
+      case CTRadioAccessTechnologyGPRS,
+        CTRadioAccessTechnologyEdge,
+      CTRadioAccessTechnologyCDMA1x:
+        return NetworkCellularGeneration.cellularGen2g
+      case CTRadioAccessTechnologyWCDMA,
+        CTRadioAccessTechnologyHSDPA,
+        CTRadioAccessTechnologyHSUPA,
+        CTRadioAccessTechnologyCDMAEVDORev0,
+        CTRadioAccessTechnologyCDMAEVDORevA,
+        CTRadioAccessTechnologyCDMAEVDORevB,
+      CTRadioAccessTechnologyeHRPD:
+        return NetworkCellularGeneration.cellularGen3g
+      case CTRadioAccessTechnologyLTE:
+        return NetworkCellularGeneration.cellularGen4g
+      default:
+        return NetworkCellularGeneration.unknown
+      }
+    }
+
+    return NetworkCellularGeneration.unknown
+  }
+
+  private func getSubnetMask() -> String? {
+    var addresses = [String: String]()
+
+    var ifaddr: UnsafeMutablePointer<ifaddrs>?
+    guard getifaddrs(&ifaddr) == 0, let firstAddr = ifaddr else {
+      return nil
+    }
+
+    for ptr in sequence(first: firstAddr, next: { $0.pointee.ifa_next }) {
+      let interface = ptr.pointee
+      let addrFamily = interface.ifa_addr.pointee.sa_family
+
+      if addrFamily == UInt8(AF_INET) {
+        let name = String(cString: interface.ifa_name)
+
+        var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+        getnameinfo(interface.ifa_addr,
+                    socklen_t(interface.ifa_addr.pointee.sa_len),
+                    &hostname,
+                    socklen_t(hostname.count),
+                    nil,
+                    socklen_t(0),
+                    NI_NUMERICHOST)
+        let address = String(cString: hostname)
+
+        var netmask = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+        getnameinfo(interface.ifa_netmask,
+                    socklen_t(interface.ifa_netmask.pointee.sa_len),
+                    &netmask,
+                    socklen_t(netmask.count),
+                    nil,
+                    socklen_t(0),
+                    NI_NUMERICHOST)
+        let subnetMask = String(cString: netmask)
+
+        addresses[name] = subnetMask
+      }
+    }
+
+    freeifaddrs(ifaddr)
+
+    return addresses["en0"]
   }
 }
--- packages/expo-network/ios/NetworkType.swift
+++ packages/expo-network/ios/NetworkType.swift
@@ -11,18 +11,39 @@ extension NWInterface.InterfaceType: CaseIterable {
 }
 
 enum NetworkType: CustomStringConvertible {
-  case unknown, wifi, none, cellular
+  case unknown, wifi, none, cellular, ethernet
 
   var description: String {
     switch self {
     case .wifi:
       return "WIFI"
+    case .ethernet:
+      return "ETHERNET"
     case .cellular:
       return "CELLULAR"
     case .unknown:
       return "UNKNOWN"
     case .none:
       return "NONE"
+    }
+  }
+}
+
+enum NetworkCellularGeneration: CustomStringConvertible {
+  case unknown, cellularGen2g, cellularGen3g, cellularGen4g, cellularGen5g
+
+  var description: String {
+    switch self {
+    case .cellularGen2g:
+      return "2G"
+    case .cellularGen3g:
+      return "3G"
+    case .cellularGen4g:
+      return "4G"
+    case .cellularGen5g:
+      return "5G"
+    case .unknown:
+      return "UNKNOWN"
     }
   }
 }
